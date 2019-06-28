0ed2b45f32dda20deb05f9d71a5b75357ca29d77
DevS <v.pozhuev@gmail.com>
2019-06-28 17:02:58 +0300

    add backend

---
+++ backend/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+const authRoutes = require('./routes/auth');
+const app = express();
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(cors());
+app.use('/api/auth', authRoutes);
+
+module.exports = app;
