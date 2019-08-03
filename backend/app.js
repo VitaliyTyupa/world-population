dd22f5060369cce76cc806d0901e347f0399d566
DevS <v.pozhuev@gmail.com>
2019-08-03 10:22:01 +0300

    added registration & authorization

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 const authRoutes = require('./routes/auth');
+const keys = require('./config/keys');
+
 const app = express();
+
+mongoose.connect(keys.mongoURL)
+    .then(() => console.log('connect from mongoose'))
+    .catch(error => console.log(error));
+
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: true}));
--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,14 +1,53 @@
-module.exports.login = (req, res) => {
-    res.status(200).json({
-        login:  {
-            email: req.body.email,
-            password: req.body.password
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+const keys = require('../config/keys');
+
+
+module.exports.login = async (req, res) => {
+    const candidate = await User.findOne({email: req.body.email});
+    if(candidate) {
+        const passwordResult = bcrypt.compareSync(req.body.password, candidate.password);
+        if (passwordResult) {
+            const token = jwt.sign({
+                email: candidate.email,
+                userId: candidate._id
+            }, keys.jwt, {expiresIn: 60 * 60});
+            res.status(200).json({
+                token: `Bearer ${token}`
+            })
+        } else {
+            res.status(401).json({
+                message: "password isn't correct, try again"
+            })
         }
-    })
+    } else {
+      res.status(404).json({
+          message: "user with this email isn't founded"
+      })
+    }
+
 };
 
-module.exports.register = (req, res) => {
-    res.status(200).json({
-        register: 'from controller'
-    })
-};
+module.exports.register = async (req, res) => {
+    const candidate = await User.findOne({email: req.body.email});
+    if(candidate) {
+        res.status(409).json({
+            message: 'This email is used'
+        })
+    } else {
+        const salt = bcrypt.genSaltSync(10);
+        const password = req.body.password;
+        const user = new User({
+            email: req.body.email,
+            password: bcrypt.hashSync(password, salt)
+        });
+        try {
+            await user.save().then(()=>{console.log('save Ok!')});
+            res.status(201).json(user)
+        } catch (e) {
+            console.log('===========', e.message);
+        }
+
+    }
+}
